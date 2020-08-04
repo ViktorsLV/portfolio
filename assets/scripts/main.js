4dce88b6e18d98b7dafa002230326a98f0e53dc0
ViktorsLV <viktors.ch11@gmail.com>
2020-08-04 16:20:39 +0300

    Emoji, section change

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -56,3 +56,27 @@ function setTheme(mode) {
     localStorage.setItem('theme', mode)
 }
 
+
+/* BATTERY CHARGE ANIMATION */
+
+function chargeBattery() {
+    let battery = document.getElementById('battery');
+    battery.innerHTML = "&#xf244;";
+    setTimeout(function(){
+        battery.innerHTML = "&#xf243;";
+    }, 1000)
+    setTimeout(function(){
+        battery.innerHTML = "&#xf242;";
+    }, 2000)
+    setTimeout(function(){
+        battery.innerHTML = "&#xf241;";
+    }, 3000)
+    setTimeout(function(){
+        battery.innerHTML = "&#xf240;";
+    }, 4000)
+}
+
+chargeBattery();
+
+// Run Animation every 5 Seconds
+setInterval(chargeBattery, 5000)
